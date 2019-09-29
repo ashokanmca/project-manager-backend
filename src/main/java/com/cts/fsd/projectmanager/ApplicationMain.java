fc105e96c565fb465cccacf63d617f17a6a8662e
kashokan-clgx <kashokan@corelogic.com>
2019-09-29 15:41:14 +0530

    Update

--- src/main/java/com/cts/fsd/projectmanager/ApplicationMain.java
+++ src/main/java/com/cts/fsd/projectmanager/ApplicationMain.java
@@ -12,11 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplicationMain extends ServletInitializer {
 	public static void main(String[] args) throws IOException {
-	
-//		set application configurations required to run the spring boot application
+		
+		//		set application configurations required to run the spring boot application
 		setSpringBootApplicationConfiguration();
 		
-//		actual method call inside the main() method to run Spring Boot Application
+		//		actual method call inside the main() method to run Spring Boot Application
 		SpringApplication.run(ApplicationMain.class, args);
 	}
 	
@@ -24,16 +24,16 @@ public class ApplicationMain extends ServletInitializer {
 	private static void setSpringBootApplicationConfiguration() {
 		
 		System.setProperty("server.servlet.context-path", "/projectmanagerbackend");
-//		System.setProperty("server.port", "8080");
-
-//		spring.datasource properties
-//		Properties related to the MySQL Database
-		System.setProperty("spring.datasource.url", "jdbc:mysql://localhost:3306/fsd_project_manager?createDatabaseIfNotExist=true");
-		System.setProperty("spring.datasource.username", "root");
-		System.setProperty("spring.datasource.password", "admin");
-		System.setProperty("spring.datasource.driverClassName", "com.mysql.jdbc.Driver");
+		//		System.setProperty("server.port", "8080");
+		
+		//		spring.datasource properties
+		//		Properties related to the MySQL Database
+		System.setProperty("spring.datasource.url", "jdbc:postgresql://localhost:5432/postgres?createDatabaseIfNotExist=true");
+		System.setProperty("spring.datasource.username", "postgres");
+		System.setProperty("spring.datasource.password", "postgres-pwd");
+		System.setProperty("spring.datasource.driverClassName", "org.postgresql.Driver");
 		System.setProperty("spring.datasource.initialize", "true");
-//		Properties related to the H2 Database
+		//		Properties related to the H2 Database
 		/*System.setProperty("spring.datasource.url", "jdbc:h2:file:~\\tmp\\fsd_project_manager");
 		System.setProperty("spring.datasource.name", "fsd_project_manager");
 		System.setProperty("spring.datasource.username", "SA");
@@ -42,18 +42,18 @@ public class ApplicationMain extends ServletInitializer {
 		System.setProperty("spring.datasource.initialize", "true");*/
 		
 		
-//		spring.jpa properties
+		//		spring.jpa properties
 		System.setProperty("spring.jpa.generate-ddl", "true");
 		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
-//		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQL5Dialect");
-//		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQLDialect");
-		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-//		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
-//		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		//		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQL5Dialect");
+		//		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
+		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		//		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
+		//		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 		System.setProperty("spring.jpa.show-sql", "true");
 		
-//		spring.h2 properties
+		//		spring.h2 properties
 		System.setProperty("spring.h2.console.enabled", "true");
 		System.setProperty("spring.h2.console.path", "/h2-console");
 	}
