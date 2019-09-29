e4f8b019f2ca83629e9775d6d49b3c861eb3de2e
kashokan-clgx <kashokan@corelogic.com>
2019-09-29 20:38:37 +0530

    Change Db from Postgres to MSSQL

--- src/main/java/com/cts/fsd/projectmanager/ApplicationMain.java
+++ src/main/java/com/cts/fsd/projectmanager/ApplicationMain.java
@@ -28,11 +28,16 @@ public class ApplicationMain extends ServletInitializer {
 		
 		//		spring.datasource properties
 		//		Properties related to the MySQL Database
-		System.setProperty("spring.datasource.url", "jdbc:postgresql://localhost:5432/postgres?createDatabaseIfNotExist=true");
-		System.setProperty("spring.datasource.username", "postgres");
-		System.setProperty("spring.datasource.password", "postgres-pwd");
-		System.setProperty("spring.datasource.driverClassName", "org.postgresql.Driver");
+		System.setProperty("spring.datasource.url", "jdbc:mysql://localhost:3306/fsd_project_manager?createDatabaseIfNotExist=true");
+		System.setProperty("spring.datasource.username", "root");
+		System.setProperty("spring.datasource.password", "admin");
+		System.setProperty("spring.datasource.driverClassName", "com.mysql.jdbc.Driver");
 		System.setProperty("spring.datasource.initialize", "true");
+//		System.setProperty("spring.datasource.url", "jdbc:postgresql://localhost:5432/postgres?createDatabaseIfNotExist=true");
+//		System.setProperty("spring.datasource.username", "postgres");
+//		System.setProperty("spring.datasource.password", "postgres-pwd");
+//		System.setProperty("spring.datasource.driverClassName", "org.postgresql.Driver");
+//		System.setProperty("spring.datasource.initialize", "true");
 		//		Properties related to the H2 Database
 		/*System.setProperty("spring.datasource.url", "jdbc:h2:file:~\\tmp\\fsd_project_manager");
 		System.setProperty("spring.datasource.name", "fsd_project_manager");
@@ -47,8 +52,10 @@ public class ApplicationMain extends ServletInitializer {
 		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
 		//		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQL5Dialect");
 		//		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
-		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQLDialect");
+		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+//		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
+//		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		//		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
 		//		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 		System.setProperty("spring.jpa.show-sql", "true");
